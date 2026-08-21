ef2a5c6f9a5f659c0331e3dcde58bcf9855c53b1
Antonio <tony.formisano.2000@gmail.com>
2026-08-21 14:31:12 +0200

    file reorganized for back-end integration

---
+++ Backend/src/main/java/CAPSTONE/LayerlyBackendApplication.java
@@ -0,0 +1,13 @@
+package CAPSTONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayerlyBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/CAPSTONE/LayerlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package CAPSTONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
